4569785aa499f68e1c8c1fd8df21e7d286fcbd52
David Nicolas Sanchez Sendoya <dnicolas.sachez@udea.edu.co>
2025-04-25 15:40:05 -0500

    refactor: change app to package

---
+++ lib/app/app.dart
@@ -0,0 +1,1 @@
+export 'widgets/my_app.dart' show MyApp;
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:my_app/home/screens/home_screen.dart';
+import 'package:my_app/app/app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
 }
