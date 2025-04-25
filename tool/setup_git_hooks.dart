124bb6da355d5ac3a5796cba3b89b2692e0cb80d
David Nicolas Sanchez Sendoya <dnicolas.sachez@udea.edu.co>
2025-04-25 16:35:40 -0500

    feat: setup pre commit

--- tool/setup_git_hooks.dart
+++ tool/setup_git_hooks.dart
@@ -5,8 +5,7 @@ Future<void> main() async {
   await preCommitHook.parent.create();
   await preCommitHook.writeAsString('''
 #!/bin/sh
-exec dart run dart_pre_commit # specify custom options here
-# exec flutter pub run dart_pre_commit # Use this instead when working on a Flutter project
+exec flutter pub run dart_pre_commit
 ''');
 
   if (!Platform.isWindows) {
